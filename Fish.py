49effd5441c441dd620dd386f5fac20cea62b380
Krishna-Sujith <krishnasujithreddy@gmail.com>
2025-01-12 04:24:22 +0530

    Updated Fish.py to draw a better looking fish. updated movement influenced by player's position

--- Fish.py
+++ Fish.py
@@ -3,7 +3,9 @@ import random
 
 SCREEN_WIDTH=1200
 SCREEN_HEIGHT=800
-FISH_COLOR = (255, 102, 0)
+FISH_COLOR=(255, 102, 0)      #orange
+TAIL_COLOR=(255, 0, 0)        #red.
+FIN_COLOR=(200,0,0)
 
 
 class Fish:
@@ -18,11 +20,11 @@ class Fish:
         self.zigzag_toggle = True 
         self.zigzag_step = random.randint(20, 50)  
 
-    def update_pos_player_based(self, x_change, y_change):
-        self.rect.x+=x_change*self.speed
-        self.rect.y+=y_change*self.speed
+    def update_pos_player_based(self, x_change, y_change,player_speed):
+        self.rect.x+=x_change*player_speed
+        self.rect.y+=y_change*player_speed
 
-    def move(self, x_change,y_change):
+    def move(self, x_change,y_change,player_speed):
         if self.direction == "straight":
             self.rect.y += self.speed
         elif self.direction == "zigzag":
@@ -38,8 +40,7 @@ class Fish:
             if self.zigzag_step <= 0:
                 self.zigzag_toggle = not self.zigzag_toggle
                 self.zigzag_step = random.randint(20, 50)
-              
-        #Handling the case when fish tries to go outside the borders of the screen.
+
         if self.rect.top > SCREEN_HEIGHT:
             self.rect.bottom = 0  
         if self.rect.right < 0:
@@ -47,7 +48,45 @@ class Fish:
         if self.rect.left > SCREEN_WIDTH:
             self.rect.right = 0  
 
-        self.update_pos_player_based(x_change,y_change)
+        self.update_pos_player_based(x_change,y_change,player_speed)
 
     def draw(self, screen):
-        pygame.draw.rect(screen, FISH_COLOR, self.rect)
+        # pygame.draw.rect(screen, FISH_COLOR, self.rect)
+        body_width = int(self.rect.width * 1.2)
+        body_height = int(self.rect.height * 1.2)
+        body_center = self.rect.center
+
+        #Drawing the fish body as an ellipse.
+        pygame.draw.ellipse(screen, FISH_COLOR, pygame.Rect(body_center[0] - body_width // 2, body_center[1] - body_height // 2, body_width, body_height))
+
+        #Drawing fish tail, reduced the size a bit.
+        tail_width = body_width // 6  
+        tail_height = body_height // 4
+        tail_points = [
+            (body_center[0] - body_width // 2, body_center[1] - tail_height),
+            (body_center[0] - body_width // 2 - tail_width, body_center[1]),
+            (body_center[0] - body_width // 2, body_center[1] + tail_height),
+        ]
+        pygame.draw.polygon(screen, FISH_COLOR, tail_points)
+
+        upper_fin_points = [
+            (body_center[0] - body_width // 4, body_center[1] - body_height // 2),
+            (body_center[0], body_center[1] - body_height // 1.5),  
+            (body_center[0] + body_width // 4, body_center[1] - body_height // 2),
+        ]
+        
+        lower_fin_points = [
+            (body_center[0] - body_width // 4, body_center[1] + body_height // 2),
+            (body_center[0], body_center[1] + body_height // 1.5), 
+            (body_center[0] + body_width // 4, body_center[1] + body_height // 2), 
+        ]
+        
+        #Drawing fins
+        pygame.draw.polygon(screen, FIN_COLOR, upper_fin_points)
+        pygame.draw.polygon(screen, FIN_COLOR, lower_fin_points)
+
+        #Drawing the eyes.
+        eye_radius = body_width // 15 
+        eye_x = body_center[0] + body_width // 4 
+        eye_y = body_center[1] - body_height // 8 
+        pygame.draw.circle(screen, (0, 0, 0), (eye_x, eye_y), eye_radius)
